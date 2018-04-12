1cac52fd93072b4a69e9bb61ff223e8be95c7757
balmeidaa <bj.aa.414@gmail.com>
2018-04-12 16:36:19 -0500

    feat(es6 exercise)

--- balmeida/almeida_notas.md
+++ balmeida/almeida_notas.md
@@ -1,17 +1,42 @@
-# course-april-2018
-
+#notas
 
 Upstream es el repositorio 'maestro'
 Origin es el repositorio de donde que esta basado en el upstream
 Local repo local
 
-##git commands
+#git commands
 
 ```sh
 git remote add origin <remote url>
 git remote add upstream <upstream_remote>
 git pull upstream <branch>
 ```
+
+crea un branch 
+
+```
+git branch <branch>
+```
+
+crea un branch y se pasa al branch en un solo comando
+```
+git checkout -b <branch_name>
+```
+
+para pasar de staged a untracked, revierte el ultimo cambio
+```
+git reset <file/folder>
+```
+
+para fusionar branch en otra, cambiar a la cual se va fusionar p.e. master se fusiona con branch
+en master:
+git merge <branch>
+
+borra un branch siempre y cuando no estes en el branchs
+```
+git branch -d <branch>
+```
+
 ##.gitignore
 
 Siempre crear el archivo .gitignore antes de hacer commit
---
+++ balmeida/ecmascript/json_server/script.js
@@ -0,0 +1,69 @@
+'use strict'
+
+let htmlTable = document.getElementById('tableData');
+
+	//adding the headers to the table
+	
+let header = htmlTable.insertRow(0);
+	
+var keyHeader = header.insertCell(0);
+var valueHeader = header.insertCell(1);
+
+	// Add some text to the headers:
+keyHeader.innerHTML = "Key";
+valueHeader.innerHTML = "Value";
+
+/*
+*@param jsonData:json 
+*@description Tries to add data to a table if available
+*/
+const createTable = (jsonData) => {
+	 
+	if(jsonData === undefined){
+		 throw Error({
+            type :'FATAL',
+            message:'No data from server'
+        });
+	}
+	
+	for (let objectJson  of jsonData){
+		
+		
+		
+		if(objectJson === 'key_x'){
+			throw Error({
+            type :'WARN',
+            message:'No valid key'
+        });
+			continue;
+		}
+			
+		let rowTable = htmlTable.insertRow(htmlTable.rows.length);
+		let keyCell = rowTable.insertCell(0);
+		let valueCell = rowTable.insertCell(1);
+		
+		let keyName = Object.keys(objectJson); 
+		
+		keyCell.innerHTML = Object.keys(objectJson);
+		valueCell.innerHTML = objectJson[keyName];
+	}
+	
+	
+	
+};
+ 
+ 
+fetch('http://localhost:3000/data')
+  .then(
+    (response)=> {
+
+	if(response.status === 200 || response.status === 304){ 		
+
+		let jsonData = response.json().then( (json) => createTable(json) );
+	
+		}
+	}
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
