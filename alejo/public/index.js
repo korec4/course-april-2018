cc9ab0fe088b6e5d827687f8cd8b1927c37b115b
Alejandro Caravantes <caravantes01@outlook.com>
2018-04-13 17:00:19 -0500

    feat(web-serve): Improvements to index.js better practices

--- alejo/public/index.js
+++ alejo/public/index.js
@@ -3,12 +3,31 @@
 (function IIFE() {
     const URL = 'https://jsonplaceholder.typicode.com/';
 
-    const getUsers = (response) =>{
-        if(response.status !== 200){
+    //message of welcome to the user
+    document.getElementById('selectMessage').innerHTML = `<span>Select a item to look:</span><br>`;
+    const select = document.getElementById('options');
+
+    /**
+     * This nethod validate a response if not code 200 write on message and return a null
+     * @param response
+     * @returns {response|null}
+     */
+    const validateResponse = (response) => {
+        if (response.status !== 200) {
             const message = document.getElementById('message');
-            message.innerText = 'Looks like there was a problem. Status code: '+ response.status;
+            message.innerText = 'Looks like there was a problem. Status code: ' + response.status;
             return;
         }
+        return response;
+    };
+
+    /**
+     * This method return a table HTML to view the data
+     * @param response
+     */
+    const getUsers = (response) => {
+
+        validateResponse(response);
 
         response.json().then((users) => {
             const message = document.getElementById('message');
@@ -16,21 +35,24 @@
             const tableData = document.getElementById('table');
             tableData.innerHTML = `<thead><tr><th>ID</th><th>Name</th><th>Email</th></tr></thead>`;
 
-            for(const user of users){
-                tableData.innerHTML +=  `<tr><th> ${user.id} </th><th> ${user.name} </th><th> ${user.email} </th></tr>`;
+            for (const user of users) {
+                tableData.innerHTML += `<tr><th> ${user.id} </th><th> ${user.name} </th><th> ${user.email} </th></tr>`;
             }
         });
     };
 
-    const getRequest = (typeObject = 'users') =>{
-        fetch(URL + typeObject)
+    /**
+     * This method require a typeResquest to request at api
+     * @param typeRequest
+     */
+    const getRequest = (typeRequest = 'users') => {
+        fetch
+        (URL + typeRequest)
             .then(response => getUsers(response))
-            .catch(function (err) {
-                const message = document.getElementById('message');
-                message.innerText = "Fetch Error : -S" + err;
+            .catch(function (error) {
+                document.getElementById('message').innerText = "Fetch Error : -S" + error;
             });
     };
-
     getRequest();
 })();
 
