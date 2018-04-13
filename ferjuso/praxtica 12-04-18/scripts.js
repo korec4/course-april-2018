9385f872e70faae4a48eb5aaeb713646d36cdb09
fersoto <fernandosotojuarez@gmail.com>
2018-04-13 17:25:38 -0500

    practica front completa

--- ferjuso/praxtica 12-04-18/scripts.js
+++ ferjuso/praxtica 12-04-18/scripts.js
@@ -1,40 +1,35 @@
 'use strict';
 
 (function IIFE(document){
+    
     document.myObject;
-    const tabla = document.querySelector('#myTable');//querychecar cuando
-    const datos = { 
-        key1: 'valor',
-        key2: 'valor2',
-        key3: 'valor3'  
-    };
-
-    //const URL = 
-   /*
-    fetch('datos.json')
-    .then(respuesta => respuesta.json())
-    .then(datos => console.log(datos.key))
+    const tabla = document.querySelector('#myTable');
+    const URL_API = 'datos.json';
+   
+    fetch(URL_API)
+    .then(response => response.json())
+    .then(response => fillTable(response.data))
     .catch(error => console.error('Hay un error'));
-   */
-    
-    
-      let finalTable = `
-    <tr>
-        <th>Mi Tabla</th>
-    </tr>
-    `;
-
-    for ( const key in datos){
-        finalTable = finalTable + `
-            <tr>
-                <td>
-                    ${key}
-                </td>
-                <td>
-                    ${datos[key]}
-                </td>
-            </tr>
+   
+    const fillTable = (data) => {
+        let finalTable = `
+        <tr>
+            <th>Mi Tabla</th>
+        </tr>
         `;
+        console.log(data);
+        for ( const object of data){
+            finalTable = finalTable + `
+                <tr>
+                    <td>
+                        ${object.nombre}
+                    </td>
+                    <td>
+                        ${object.mascota}
+                    </td>
+                </tr>
+            `;
+        }
+        tabla.innerHTML = finalTable;
     }
-    tabla.innerHTML = finalTable;
 })(document);
