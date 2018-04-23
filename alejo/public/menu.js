656241ca5008b8c49b5c40e7f482bc30caf23847
Alejandro Caravantes <caravantes01@outlook.com>
2018-04-22 19:00:44 -0500

    chore(test): added practice web responsive

---
+++ alejo/public/menu.js
@@ -0,0 +1,14 @@
+'use strict';
+(function () {
+
+    const navbarButtonMobile = document.querySelector('.navbar__button-mobile');
+    const sidenav = document.querySelector('.sidenav');
+    const sidenavClose = document.querySelector('.sidenav-close');
+    navbarButtonMobile.addEventListener('click', () => {
+        sidenav.classList.add('sidenav-active');
+    });
+
+    sidenavClose.addEventListener('click', () =>{
+       sidenav.classList.remove('sidenav-active');
+    });
+})();
