aab1756acb4c726f665d0293d14d1b4ecfba6b8c
Alejandro Caravantes <caravantes01@outlook.com>
2018-05-04 15:01:20 -0500

    feat(front): feat style

--- alejo/front/app.js
+++ alejo/front/app.js
@@ -10,15 +10,15 @@
      * this method added a listen action when a user click on discover button
      */
     buttonDiscover.addEventListener('click', () => {
-        document.getElementById('description').innerHTML = `<div class="message"><b>Encuentra tus libros favoritos</b> en academy con mas de 200,000 audiolibros disponibles</div>`;
+        document.getElementById('description').innerHTML = `<div class="message-mobile"><b>Encuentra tus libros favoritos</b> en academy con mas de 200,000 audiolibros disponibles</div>`;
     });
 
     buttonDownload.addEventListener('click', () => {
-       document.getElementById('description').innerHTML =  `<div class="message"><b>Lleva contigo siempre</b> tus libros favoritos y no pares de escuchar</div>`;
+       document.getElementById('description').innerHTML =  `<div class="message-mobile"><b>Lleva contigo siempre</b> tus libros favoritos y no pares de escuchar</div>`;
     });
 
     buttonListen.addEventListener('click', () => {
-       document.getElementById('description').innerHTML =  `<div class="message">Disfruta de tus libros por 299 al mes en <b>Central Academy</b></div>`;
+       document.getElementById('description').innerHTML =  `<div class="message-mobile">Disfruta de tus libros por 299 al mes en <b>Central Academy</b></div>`;
     });
 
     buttonModal.addEventListener('click', () => {
